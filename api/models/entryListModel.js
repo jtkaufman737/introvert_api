8dc503e0443bed15a8a6deab4f89ca76e5864765
JT Kaufman <jtkaufman737@gmail.com>
2019-07-29 19:22:44 -0400

    New packages and mongoose schema

---
+++ api/models/entryListModel.js
@@ -0,0 +1,25 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var reqMsg = 'This field is required';
+
+var EntrySchema = new Schema({
+  submitter: {
+    type: String,
+    required: reqMsg
+  },
+  attributedTo: {
+    type: String
+  },
+  body: {
+    type: String,
+    required: reqMsg
+  },
+  tags: {
+    type: Array
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now 
+  }
+})
